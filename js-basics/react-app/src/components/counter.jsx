bce98ac9bbb1215297c708fa3b0e06af420697a6
winfred <winfred242002@gmail.com>
2023-03-16 16:14:29 +0000

    ♻ conditional list rendering

--- js-basics/react-app/src/components/counter.jsx
+++ js-basics/react-app/src/components/counter.jsx
@@ -12,14 +12,22 @@ class Counter extends Component {
         color: 'blue'
     }
 
+    listCheck() {
+        if (this.state.tags.length === 0) {
+            return (<h3>There are zero List items</h3>);
+        } else {
+            return (this.state.tags.map(tag => <li key={tag}>{tag}</li>));
+        }
+    }
+
     render() {
         return (
             /// using React.Fragment to prevent numerous <div>s
             <React.Fragment>
-                <img src={this.state.imageUrl} alt={'Random image'} />
+                <img src={this.state.imageUrl} alt={'Random image'}/>
                 <span style={this.styles} className="badge badge-warning m-2">Hello {this.formatCount()}</span>
                 <button className="btn btn-secondary">Increase</button>
-                <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+                <div>{this.listCheck()}</div>
             </React.Fragment>
         );
     }
