91920a2a83a33e1d545d4c310b2c8654567f04ed
winfred <winfred242002@gmail.com>
2023-03-16 13:40:04 +0000

    💫 create counter component

---
+++ js-basics/react-app/src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Counter extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Hello Winfred</h1>
+                <button>Increase</button>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Counter;
