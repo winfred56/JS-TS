9e008c386440b6fcd1fee1c392fd37cf998872cd
winfred <winfred242002@gmail.com>
2023-05-04 09:25:39 +0000

    🚀 hello world code

---
+++ react/hello-world/src/components/Welcome.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+class Welcome extends Component {
+    render() {
+        return <h3>Welcome</h3>
+    }
+}
+export default Welcome
---
+++ react/hello-world/src/components/greet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// function Greet() {
+//     return <h1>Hello, Today is Wednesday</h1>
+// }
+const Greet = () => <h1>Hello, Today is Wednesday</h1>
+export default Greet
