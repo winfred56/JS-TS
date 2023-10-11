ab531191134f9a02a59994e5c8d3c3aac77b4dc8
winfred <winfred242002@gmail.com>
2023-10-11 11:05:30 +0000

    :bug: extend custom css

--- tailTut/tailwind.config.js
+++ tailTut/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FF6363",
+                secondary: {
+                    100: "#E2E2D5",
+                    200: "#BBBBBE"
+                }
+            },
+            fontFamily: {body: 'Nunito Sans'}
+        },
+    },
+    plugins: [],
 }
 
