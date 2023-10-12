3a04282459ba3dbe7f2d1b16742c9e640183c363
winfred <winfred242002@gmail.com>
2023-10-12 16:03:34 +0000

    :100: stats

--- oobank/vite-project/src/components/GetStarted.jsx
+++ oobank/vite-project/src/components/GetStarted.jsx
@@ -1,8 +1,23 @@
 
 import React from 'react';
+import style from "../style.js";
+import {arrowUp} from "../assets"
 
 const GetStarted = () => (
-    <div className="">GetStarted</div>
+    <div className={`${style.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient cursor-pointer p-[2px]`}>
+        <div className={`${style.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+            <div className={`${style.flexStart} flex-row`}>
+                <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+                    <span className="text-gradient">Get</span>
+                </p>
+                <img src={arrowUp} className="w-[23px] h-[23px] object-contain" alt="arrowUp"/>
+
+            </div>
+            <p className="font-poppins font-medium text-[18px] leading-[23px]">
+                <span className="text-gradient">Started</span>
+            </p>
+        </div>
+    </div>
 );
 
 export default GetStarted
--- oobank/vite-project/src/components/Hero.jsx
+++ oobank/vite-project/src/components/Hero.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import style from "../style.js";
+import {discount, robot} from "../assets"
+import GetStarted from "./GetStarted.jsx";
 
 const Hero = () => (
-    <div className="">Hero</div>
+    <section id="home" className={`flex md:flex-row flex-col ${style.paddingY}`}>
+        <div className={`flex-1 ${style.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+            <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+                <img src={discount} alt="discount" className="w-[32px]"/>
+                <p className={`${style.paragraph} ml-2`}>
+                    <span className="text-white">20%</span> Discount For {" "}
+                    <span className="text-white">1 Month</span> Account
+                </p>
+            </div>
+
+            <div className="flex flex-row justify-between items-center w-full">
+                <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+                    The Next <br className="sm:block hidden"/>{" "}
+                    <span
+                    className="text-gradient capitalize">generation</span>
+                    {" "} </h1>
+                <div className="ss:flex hidden md:mr-4 mr-0">
+                    <GetStarted />
+                </div>
+            </div>
+            <h1 className="font-poppins font-semibold ss:text-[68px] w-full text-[52px] text-white ss:leading-[100px] leading-[75px]">
+            Payment Method
+            </h1>
+            <p className={`${style.paragraph} max-w-[470px] mt-5`}>Lorem ipsum, consectetur adipiscing elit. Vivamus id mattis ligula. Suspendisse potenti. In hac platea dictumst. Ut eget nibh ipsum. In ac elit vitae tellus ultrices facilisis. Curabitur sed vestibulum ex. Donec tincidunt leo diam, ut auctor augue dignissim ac. Vivamus et orci in nulla auctor blandit.</p>
+        </div>
+        <div className={`flex flex-1 ${style.flexCenter} md:my-0 my-10 relative`}>
+            <img className="w-[100%] h-[100%] relative z-[5]" src={robot} alt="robot"/>
+            <div className="absolute z-[1] w-[80%] h-[80%]  rounded-full bottom-40 white__gradient"></div>
+            <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
+            <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"></div>
+        </div>
+        <div className={`ss:hidden ${style.flexCenter}`}><GetStarted /></div>
+
+    </section>
 );
 
 export default Hero
