1c68dc380d36c4f090cc6a80adac503910fff357
winfred <winfred242002@gmail.com>
2023-10-21 11:22:44 +0000

    first component

---
+++ first_react_p/src/components/Hello.js
@@ -0,0 +1,9 @@
+/// Using the javascript syntax for creating a function
+// function Hello (){
+//     return <h1>Hello World</h1>
+// }
+
+/// Using ES6 syntax for creating a function
+const Hello = () => <h1>Hello World!</h1>;
+
+export default Hello;
