b28c097171066d381ae0972c4c019f6d5ce0bcc7
winfred <winfred242002@gmail.com>
2023-10-23 09:24:14 +0000

    conditional rendering

--- first_react_p/src/components/ConditionalComponent.js
+++ first_react_p/src/components/ConditionalComponent.js
@@ -3,20 +3,17 @@ import {useState} from "react";
 export default function ConditionalComponent() {
     const [display, setDisplay] = useState(false)
     const alterState = () => setDisplay(!display);
+    let outputDisplay;
     if(display){
-        return (
-            <div>
-                <h1>Display is currently set to True.</h1>
-                <button onClick={alterState}>Click me</button>
-            </div>
-        )
+        outputDisplay = <h1>Display is currently set to True.</h1>
     }
     else {
-        return (
-            <div>
-                <h1>Display is now set to False.</h1>
-                <button onClick={alterState}>Click me</button>
-            </div>
-        )
+        outputDisplay = <h1>Display is now set to False.</h1>
     }
+    return(
+        <div>
+            {outputDisplay}
+            <button onClick={alterState}>Click me</button>
+        </div>
+    )
 }
