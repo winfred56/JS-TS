b6ead6091d014ecf99d679c2a2bedd49e88db894
winfred <winfred242002@gmail.com>
2023-10-26 18:17:13 +0000

    :fire:

--- test-2/src/components/Hero.js
+++ test-2/src/components/Hero.js
@@ -1,9 +1,10 @@
+import {Link} from 'react-router-dom'
 export default function Hero(){
     return (
         <div className="py-20 md:px-[650px] md:py-50">
-            <p className="font-Headings text-4xl md:text-6xl font-medium">Discover the beauty around the world</p>
+            <h1 className="font-Headings text-4xl md:text-6xl font-semibold">Discover the beauty around the world</h1>
             <p className="font-Subtitles text-sm md:text-base py-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget diam.</p>
-            <button type="button" className="bg-black text-white rounded-md px-4 py-2">Explore</button>
+            <Link to="/explore"><button type="button" className="bg-black text-white rounded-md px-4 py-2 hover:rotate-6">Explore</button></Link>
         </div>
     )
 }
