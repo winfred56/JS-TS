55923d07a68efed7a15a78cbad5159e294969fc8
winfred <winfred242002@gmail.com>
2023-10-30 09:40:28 +0000

    :100: clients

--- acme/src/components/Clients.js
+++ acme/src/components/Clients.js
@@ -1,10 +1,13 @@
 import klassic from "./../assets/klassic.png"
-import kat from "./../assets/kat.png"
+import kat from "./../assets/kath.png"
 import lamerta from "./../assets/lamerta.png"
 import vivo from "./../assets/vivo.png"
 import otuasekan from "./../assets/otuasekan.png"
 import arcc from "./../assets/arcc.png"
-export default function Clients(){
+import effiduase from "./../assets/effiduase.png"
+import aboafo from "./../assets/aboafo.png"
+
+export default function Clients() {
     const clients = [
         {src: lamerta, alt: "", width: "200", height: "00"},
         {src: klassic, alt: "", width: "200", height: "100"},
@@ -12,19 +15,24 @@ export default function Clients(){
         {src: vivo, alt: "", width: "200", height: "100"},
         {src: arcc, alt: "", width: "200", height: "100"},
         {src: otuasekan, alt: "", width: "200", height: "100"},
-
+        {src: effiduase, alt: "", width: "200", height: "100"},
+        {src: aboafo, alt: "", width: "200", height: "100"},
 
     ]
     return (
         <div className="bg-background text-center text-sm md:text-base md:pt-10 px-5 md:px-20 text-body-text">
             <p className="font-body-text">TRUSTED BY CLIENTS FROM TOP COMPANIES</p>
-            <div className="scroller">
-                <ul className="tag_list scroller__inner">
-                    {clients.map((client) =>(
-                        <li key={client.src} className=""><img src={client.src} width={client.width} alt={client.alt} className=""/></li>
-                    ))}
-                </ul>
+            <div className="container mx-auto flex flex-wrap justify-center items-center my-5">
+                <div className="carousel-container-clients carousel-fade-clients">
+                    <div className="carousel-clients space-x-16">
+                        <ul className="content flex gap-4">
+                            {clients.map((client) =>(
+                                <li key={client.src} className=""><img src={client.src} width={client.width} height={client.height} alt={client.alt} className=""/></li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+)
 }
