e35a474e637aca55a27822413b96ba02db6b0eaa
winfred <winfred242002@gmail.com>
2023-11-07 21:25:08 +0000

    🧹 chore: nav

--- common_app1/tailwind.config.ts
+++ common_app1/tailwind.config.ts
@@ -9,9 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        // 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        // 'gradient-conic':
+        //   'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'hero-img': "url('/home_page_hero_optimized.jpg')",
       },
       fontFamily:{
         'body-text':['Karla', 'sans-serif'],
