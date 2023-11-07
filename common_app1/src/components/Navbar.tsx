c69add3e192ded91a38803f58841103052327c85
winfred <winfred242002@gmail.com>
2023-11-07 16:31:01 +0000

    🧹 chore: nav

--- common_app1/src/components/Navbar.tsx
+++ common_app1/src/components/Navbar.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import commonAppLogo from "../../public/common-app-logo.svg";
+import {useState} from "react";
+import NavMenu from "@/components/NavMenu";
+import NavCTAButtons from "@/components/NavCTAButtons";
+
 export default function Navbar(){
-    return <nav className={`mx-auto`}>
-        welcome
-    </nav>
+
+    return (
+        <nav className={`mx-auto flex flex-row items-center`}>
+            <Image src={commonAppLogo} alt={`commonAppLogo`} className={`w-[100px] md:w-[170px]`} />
+            <NavMenu />
+
+            <NavCTAButtons  />
+
+        </nav>
+    )
 }
