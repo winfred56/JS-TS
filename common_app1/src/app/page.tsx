324f50ade27aefffc3548477249da676b047562c
winfred <winfred242002@gmail.com>
2023-11-07 21:24:44 +0000

    🧹 chore: nav

--- common_app1/src/app/page.tsx
+++ common_app1/src/app/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+import hero_image from "../../public/home_page_hero_optimized.jpg";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="font-body-text">
+        <header className={`hero-bg bg-cover font-body-text relative`}>
+            <Navbar />
+        </header>
 
     </main>
   )
--- common_app1/src/components/NavCTAButtons.tsx
+++ common_app1/src/components/NavCTAButtons.tsx
@@ -1,8 +1,8 @@
-export default function NavCTAButtons(){
-    return (
-        <div className="w-full font-body-text flex flex-row gap-5">
-            <button className="bg-white hover:bg-opacity-60 font-semibold px-6 py-3 rounded-full">Sign in</button>
-            <button className="font-semibold bg-black bg-opacity-70 hover:bg-opacity-40 text-white px-6 py-3 rounded-full">create an account</button>
-        </div>
-    )
-}
+// export default function NavCTAButtons(){
+//     return (
+//         // <div className="min-w-max justify-end font-body-text hidden lg:flex flex-row gap-2">
+//         //     <button className="bg-white hover:bg-opacity-60 font-semibold px-6 py-3 rounded-full">Sign in</button>
+//         //     <button className="font-semibold bg-black bg-opacity-70 hover:bg-opacity-40 text-white px-6 py-3 rounded-full">create an account</button>
+//         // </div>
+//     )
+// }
