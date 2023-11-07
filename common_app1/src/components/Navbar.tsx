de447c974bf2fb82d201206cb91de643c0d1483c
winfred <winfred242002@gmail.com>
2023-11-07 09:03:44 +0000

    🎨 style: assets

---
+++ common_app1/src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+export default function Navbar(){
+    return <nav className={`mx-auto`}>
+        welcome
+    </nav>
+}
---
+++ common_app1/tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily:{
+        'body-text':['Karla', 'sans-serif'],
+      },
+      colors:{
+        // 'body-text': '#898989'
+      },
+      textColor:{
+        // 'body-text': '#898989'
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
