392e56e053760ea31001126d915f01d240c82307
winfred <winfred242002@gmail.com>
2023-11-07 16:30:36 +0000

    🧹 chore: nav buttons

---
+++ common_app1/src/components/NavCTAButtons.tsx
@@ -0,0 +1,8 @@
+export default function NavCTAButtons(){
+    return (
+        <div className="w-full font-body-text flex flex-row gap-5">
+            <button className="bg-white hover:bg-opacity-60 font-semibold px-6 py-3 rounded-full">Sign in</button>
+            <button className="font-semibold bg-black bg-opacity-70 hover:bg-opacity-40 text-white px-6 py-3 rounded-full">create an account</button>
+        </div>
+    )
+}
