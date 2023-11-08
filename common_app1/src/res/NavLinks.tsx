b0f758a435047a771acc002a62fdba1842d1d31d
winfred <winfred242002@gmail.com>
2023-11-08 09:34:47 +0000

    🧹 chore: mobile nav

---
+++ common_app1/src/res/NavLinks.tsx
@@ -0,0 +1,33 @@
+export const NavMenuItems = [
+    {
+        navName: "Plan for college",
+        navNameLink: "/plan",
+        subMenu: [
+            {subNavName: "Why college matters", subMenuLink: "/plan/why-college-matters"},
+            {subNavName: "Paying for college", subMenuLink: "/plan/paying-for-college"},
+            {subNavName: "Your path to college", subMenuLink: "/plan/your-path-to-college"},
+            {subNavName: "Family resources", subMenuLink: "/plan/for-families"},
+        ],
+    },
+    {
+        navName: "Apply to college",
+        navNameLink: "/apply",
+        subMenu: [
+            {subNavName: "First-year application guide", subMenuLink: "/apply/first-year-students"},
+            {subNavName: "First-year essay prompts", subMenuLink: "/apply/essay-prompts"},
+            {subNavName: "Transfer application guide", subMenuLink: "/apply/transfer-students"},
+            {subNavName: "Download mobile app", subMenuLink: "/mobile"},
+        ],
+    },
+    {
+        navName: "Support your students",
+        navNameLink: "/counselors-and-recommenders",
+        subMenu: [
+            {subNavName: "Recommender system", subMenuLink: "/counselors-and-recommenders/recommender-system"},
+            {subNavName: "Common App Ready toolkit", subMenuLink: "/counselors-and-recommenders/recommender-guide"},
+            {subNavName: "Recommender guide", subMenuLink: "/counselors-and-recommenders/recommender-guide"},
+            {subNavName: "Transfer advisors", subMenuLink: "/transfer-counselors-and-advisors"},
+            {subNavName: "News and updates", subMenuLink: "/blog"},
+        ],
+    },
+];
