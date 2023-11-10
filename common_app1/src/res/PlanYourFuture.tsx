0a67dd622d10db91fb6bd426d263f92c449b25d2
winfred <winfred242002@gmail.com>
2023-11-10 11:07:30 +0000

    🧹 chore:

---
+++ common_app1/src/res/PlanYourFuture.tsx
@@ -0,0 +1,27 @@
+import icon_direction from "./../../public/icon-direction.svg"
+import calculator from "./../../public/icon-calculator.svg"
+import desktop from "./../../public/Desktop-R4.svg"
+
+export const planYourFuture = [
+    {
+        icon: icon_direction,
+        title: 'WHY COLLEGE MATTERS',
+        description: 'College is worth it. Your future is worth it. You are worth it.',
+        linkText: 'Learn why college matters',
+        linkAddress: ''
+    },
+    {
+        icon: calculator,
+        title: 'PAYING FOR COLLEGE',
+        description: 'There are lots of options available to you. We can help you find them.',
+        linkText: 'Learn how to pay for college',
+        linkAddress: ''
+    },
+    {
+        icon: desktop,
+        title: 'YOUR COLLEGE ROADMAP',
+        description: 'The path may seem unclear. We can help you find your way.',
+        linkText: 'Learn how to get to college',
+        linkAddress: ''
+    },
+]
