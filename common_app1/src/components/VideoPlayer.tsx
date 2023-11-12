e0221ecc4beea2d07de5f80c219ea2179e9c6fd2
winfred <winfred242002@gmail.com>
2023-11-12 08:38:35 +0000

    :recycle: Refactor

--- common_app1/src/components/VideoPlayer.tsx
+++ common_app1/src/components/VideoPlayer.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { useState } from 'react';
-import homeVideo from './../../public/home-video_optimized.png'
-import Image from "next/image";
+import Link from "next/link";
 
 export default function VideoPlayer() {
     const [isPlaying, setIsPlaying] = useState(false);
@@ -21,20 +20,17 @@ export default function VideoPlayer() {
                 onClick={playVideo}
                 className="cursor-pointer flex flex-col xl:flex-row gap-10 w-full"
             >
-                <div className={`relative`}>
-                    <Image
-                        src={homeVideo}
-                        alt="Custom Thumbnail"
-                        className="w-[80rem] object-cover"
-                    />
-                    {!isPlaying && (
-                        <div className="absolute inset-0 flex items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="flex items-center justify-center text-white w-14 h-14">
-                                <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
-                            </svg>
-                        </div>
-                    )}
-                </div>
+                <video
+                    id="video"
+                    controls={true}
+                    autoPlay={false}
+                    muted={true}
+                    poster="/home-video_optimized.png"
+                    src="https://www.youtube.com/watch?v=KGywXmB_Mwo"
+                    className={`flex top-0 left-0 xl:w-[36rem] object-cover`}
+                >
+                    Your browser does not support the video tag.
+                </video>
                 <div className="">
                     <h2 className={`font-extrabold text-xl md:text-5xl`}>
                         The world is ready for you.<br className={``}/>Be ready for the world.
@@ -43,7 +39,7 @@ export default function VideoPlayer() {
                         Common App and Reach Higher have united to inspire more people to complete their education and own their future, no matter what it holds. Get valuable advice from students who have been in your shoes.
                     </p>
                     <button className="bg-[#0b6dbd] mt-10 w-full sm:w-fit md:mt-16 text-white hover:bg-opacity-60 font-semibold text-sm px-6 py-3 rounded-full">
-                        learn about our access initiatives
+                        <Link href={`/about/access-equity`}>learn about our access initiatives</Link>
                     </button>
                 </div>
             </div>
@@ -55,7 +51,7 @@ export default function VideoPlayer() {
                 muted={true}
                 poster="/home-video_optimized.png"
                 src="https://www.youtube.com/watch?v=KGywXmB_Mwo"
-                className={`absolute top-0 left-0 w-[80rem] object-cover ${isPlaying ? 'block' : 'hidden'}`}
+                className={`absolute top-0 left-0 xl:w-[36rem] object-cover ${isPlaying ? 'block' : 'hidden'}`}
             >
                 Your browser does not support the video tag.
             </video>
