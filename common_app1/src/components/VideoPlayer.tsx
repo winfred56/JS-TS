e37e95d60d06480286711dc80e7efe5f8d113860
winfred <winfred242002@gmail.com>
2023-11-12 09:29:24 +0000

    :recycle: Refactor

--- common_app1/src/components/VideoPlayer.tsx
+++ common_app1/src/components/VideoPlayer.tsx
@@ -20,17 +20,26 @@ export default function VideoPlayer() {
                 onClick={playVideo}
                 className="cursor-pointer flex flex-col xl:flex-row gap-10 w-full"
             >
-                <video
-                    id="video"
-                    controls={true}
-                    autoPlay={false}
-                    muted={true}
-                    poster="/home-video_optimized.png"
-                    src="https://www.youtube.com/watch?v=KGywXmB_Mwo"
-                    className={`flex top-0 left-0 xl:w-[36rem] object-cover`}
-                >
-                    Your browser does not support the video tag.
-                </video>
+                <div className={`relative w-full`}>
+                    <video
+                        id="video"
+                        controls={isPlaying}
+                        autoPlay={false}
+                        muted={true}
+                        poster="/home-video_optimized.png"
+                        src="https://www.youtube.com/watch?v=KGywXmB_Mwo"
+                        className={`flex top-0 left-0 xl:w-[36rem] object-cover`}
+                    >
+                        Your browser does not support the video tag.
+                    </video>
+                    {!isPlaying && (
+                        <div className="absolute left-0 inset-0 flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="flex items-center justify-center text-white w-14 h-14">
+                                <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
+                            </svg>
+                        </div>
+                    )}
+                </div>
                 <div className="">
                     <h2 className={`font-extrabold text-xl md:text-5xl`}>
                         The world is ready for you.<br className={``}/>Be ready for the world.
